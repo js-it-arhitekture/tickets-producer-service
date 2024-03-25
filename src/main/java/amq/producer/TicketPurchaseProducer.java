84c36357458c25d0fee652abab486df9583bba8b
= <jan.sernec@gmail.com>
2024-03-25 15:10:07 +0100

    removed artemis

--- src/main/java/amq/producer/TicketPurchaseProducer.java
+++ src/main/java/amq/producer/TicketPurchaseProducer.java
@@ -1,26 +1,29 @@
 package amq.producer;
 
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSContext;
-import javax.jms.Queue;
-import javax.jms.TextMessage;
+import jakarta.jms.ConnectionFactory;
+import jakarta.jms.JMSContext;
+import jakarta.jms.Queue;
+import jakarta.jms.TextMessage;
 
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class TicketPurchaseProducer {
 
+    private static final Logger LOG = Logger.getLogger(TicketPurchaseProducer.class.getName());
     @Inject
     ConnectionFactory connectionFactory;
 
     public void sendMessageToQueue(String queueName, String messageContent) {
         try (JMSContext context = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+
             Queue queue = context.createQueue(queueName);
             TextMessage message = context.createTextMessage(messageContent);
             context.createProducer().send(queue, message);
+            LOG.info("logging send!");
         }
     }
 }
